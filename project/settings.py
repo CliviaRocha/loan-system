c17368e2172f1d3b2f3f265f7b6455af8250fab7
CliviaRocha <clivia.lar@gmail.com>
2019-05-22 10:07:08 -0300

    Alterado servidor de banco

--- project/settings.py
+++ project/settings.py
@@ -91,8 +91,12 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'dbfintech',
+        'USER': 'postgres',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
